cdbffd3759f4fb14eb79efe73b6686a06f92f9da
Jeffrey Wong <jwong@dayspring-tech.com>
2017-12-05 10:34:59 -0800

    functions to create, get, and delete

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,23 +1,80 @@
-
-
-
 from __future__ import print_function
 
 import json
+import boto3
+import os
+import uuid
 
+
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+
+print("Using table: "+os.getenv("TABLE_NAME"))
+table = dynamodb.Table(os.getenv("TABLE_NAME"))
+print(table.creation_date_time)
 
 print('Loading function')
 
 
-def lambda_handler(event, context):
+def get(event, context):
     print("Received event: " + json.dumps(event, indent=2))
     print("value1 = " + event['resource'])
     print("value2 = " + event['pathParameters']['resourceId'])
+
+    response = table.get_item(
+        Key={
+            'uuid': event['pathParameters']['resourceId']
+        }
+    )
+    item = response['Item']
+    print(item)
+    
     return {
         'statusCode': 200,
-        'body': '["hello world"]',
+        'body': json.dumps(item),
         'headers': {
             'Content-Type': 'application/json',
         },
     }
+
+def post(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+    print("value1 = " + event['resource'])
+
+    body = json.loads(event['body'])
+    print(body)
+
+    itemUuid = uuid.uuid4().hex
+    print ("uuid = " + itemUuid)
+
+    table.put_item(
+        Item={
+            'uuid': itemUuid,
+            'content': body['content']
+        }
+    )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'uuid':itemUuid}),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+    
+def delete(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+    print("value1 = " + event['resource'])
+    print("value2 = " + event['pathParameters']['resourceId'])
+
+    response = table.delete_item(
+        Key={
+            'uuid': event['pathParameters']['resourceId']
+        }
+    )
+    print(response)
+    
+    return {
+        'statusCode': response['ResponseMetadata']['HTTPStatusCode']
+    }    
     
