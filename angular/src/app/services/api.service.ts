adcf64c87e9a7e8a8de2724557ff8b4f627853a4
Jeffrey Wong <jwong@dayspring-tech.com>
2017-12-05 17:26:09 -0800

    remove swagger, and now it works!

--- angular/src/app/services/api.service.ts
+++ angular/src/app/services/api.service.ts
@@ -14,7 +14,8 @@ export class ApiService {
 
     constructor(private http: Http) {
         if (process.env.ENV === 'production') {
-            this.apiBaseUrl = 'https://qg9z9owfpk.execute-api.us-east-1.amazonaws.com/Prod';
+            this.apiBaseUrl = 'https://ws6rexe883.execute-api.us-east-1.amazonaws.com/Prod/';
+            
         } else {
             this.apiBaseUrl = 'http://localhost:3000';
         }
